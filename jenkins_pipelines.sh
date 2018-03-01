0f9aabc4cd20d4e80017e8d9cf5e4aa0e8bdcbb0
iturner <ian.2.turner@atos.net>
2018-03-01 15:22:34 +0000

    Updates for OpenShift standalone server testing.

--- jenkins_pipelines.sh
+++ jenkins_pipelines.sh
@@ -3,4 +3,5 @@
 ## Setup up the pipeline builds
 oc project cicd
 oc new-build https://github.com/prometeo-cloud/prometeo --strategy=pipeline --name="prometeo-pipeline"
-oc new-build https://github.com/iturner/prometeo_web --strategy=pipeline --name="prometeoweb-pipeline"
+sleep 30
+oc new-build https://github.com/prometeo-cloud/prometeo_web --strategy=pipeline --name="prometeoweb-pipeline"
