7fb19fc008ce03da12c4b1fb6574ab62aa97c71f
Adam Jackson <adam@adamjackson.org.uk>
2018-03-12 22:27:01 +0000

    Changed build repo to point to personal github repo

--- jenkins_pipelines.sh
+++ jenkins_pipelines.sh
@@ -2,6 +2,6 @@
 
 ## Setup up the pipeline builds
 oc project cicd
-oc new-build https://github.com/prometeo-cloud/prometeo --strategy=pipeline --name="prometeo-pipeline"
+oc new-build https://github.com/adamecjackson/prometeo --strategy=pipeline --name="prometeo-pipeline"
 sleep 30
-oc new-build https://github.com/prometeo-cloud/prometeo_web --strategy=pipeline --name="prometeoweb-pipeline"
+oc new-build https://github.com/adamecjackson/prometeo_web --strategy=pipeline --name="prometeoweb-pipeline"
